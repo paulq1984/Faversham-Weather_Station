d2790f57a473c6c2fe9f9c93a72b6be60d700ec9
paulq1984 <paulquinnell@gmail.com>
2024-06-02 16:26:21 -0400

    Initial Commit

---
+++ FavershamWeatherStation/FavershamWeatherStationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavershamWeatherStationApp.swift
+//  FavershamWeatherStation
+//
+//  Created by PaulQ on 2024-06-02.
+//
+
+import SwiftUI
+
+@main
+struct FavershamWeatherStationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
