e1c328eedb85bfcec56f034a22e2722f4f5dbaf6
paulq1984 <paulquinnell@gmail.com>
2024-07-19 14:57:33 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,95 @@
+const PORT = 8000;
+const axios = require('axios');
+const cheerio = require('cheerio');
+const express = require('express');
+
+const app = express();
+const cors = require('cors');
+app.use(cors());
+
+const url = 'https://favershamweather.org/pwsWD/index.php';
+
+app.get('/', (req, res) => {
+  res.json('This is my webscraper');
+});
+
+app.get('/data', (req, res) => {
+  axios(url).then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const weather = [];
+    const currentTempObject = {};
+    const currentWindObject = {};
+    const forecast = $('#positionlast > div').text();
+    const currentTemp = $(
+      '#position11 > div.PWS_module_content > div.PWS_middle > div > div > b:nth-child(1)'
+    ).text();
+    const highTemp = $(
+      '#position11 > div.PWS_module_content > div.PWS_middle > div > div > b:nth-child(3)'
+    ).text();
+    const lowTemp = $(
+      '#position11 > div.PWS_module_content > div.PWS_middle > div > div > b:nth-child(4)'
+    ).text();
+    const feelsLike = $(
+      '#position11 > div.PWS_module_content > div.PWS_right > div:nth-child(1) > b'
+    ).text();
+    const currentHumidity = $(
+      '#position11 > div.PWS_module_content > div.PWS_left > div:nth-child(3) > b'
+    ).text();
+    const currentDewpoint = $(
+      '#position11 > div.PWS_module_content > div.PWS_right > div:nth-child(3) > b'
+    ).text();
+    const currentSky = $(
+      '#position13 > table > tbody > tr:nth-child(1) > td:nth-child(2)'
+    ).text();
+    const currentWindSpeed = $(
+      '#position21 > div.PWS_module_content > div.PWS_middle > div.narrow > table > tbody > tr:nth-child(1) > td:nth-child(1)'
+    ).text();
+    const currentWindGust = $(
+      '#position21 > div.PWS_module_content > div.PWS_middle > div.narrow > table > tbody > tr:nth-child(1) > td:nth-child(2) > b'
+    ).text();
+    const currentWindDirection = $(
+      '#position21 > div.PWS_module_content > div.PWS_left > div:nth-child(3) > b'
+    ).text();
+    const currentUV = $(
+      '#position14 > div.PWS_module_content > div.PWS_middle > div.PWS_bar > div'
+    ).text();
+    const currentPressure = $(
+      '#position22 > div.PWS_module_content > div.PWS_middle > div.narrow > span'
+    ).text();
+
+    const lastHourRainFall = $(
+      '#position31 > div.PWS_module_content > div.PWS_right > div:nth-child(1) > b'
+    ).text();
+
+    const airQuality = $('#position33 > div.PWS_middle > b').text();
+
+    const currentImage = $('#position44 > a > img').attr('src');
+
+    currentTempObject.temp = currentTemp;
+    currentTempObject.humidity = currentHumidity;
+    currentTempObject.dewpoint = currentDewpoint;
+    currentTempObject.highTemp = highTemp;
+    currentTempObject.lowTemp = lowTemp;
+    currentTempObject.feelsLike = feelsLike;
+
+    currentWindObject.windSpeed = currentWindSpeed;
+    currentWindObject.gust = currentWindGust;
+    currentWindObject.direction = currentWindDirection;
+
+    weather.push({
+      temperature: currentTempObject,
+      sky: currentSky,
+      wind: currentWindObject,
+      uv: currentUV,
+      pressure: currentPressure,
+      rainfall: lastHourRainFall,
+      airQuality: airQuality,
+      image: currentImage,
+      forecast: forecast,
+    });
+    res.json(weather);
+  });
+});
+
+app.listen(PORT, () => console.log(`Server Running on PORT ${PORT}`));
---
+++ src/app.js
@@ -0,0 +1,45 @@
+const currentTemp = document.querySelector('#currentTemp');
+const currentSky = document.querySelector('#currentSky');
+const tempInfo = document.querySelector('#tempInfo');
+const airQinfo = document.querySelector('#airQ');
+const uvIndex = document.querySelector('#currentUV');
+const humidity = document.querySelector('#currentHum');
+const wind = document.querySelector('#currentWind');
+const dewpoint = document.querySelector('#currentDew');
+const pressure = document.querySelector('#currentPressure');
+const rainfall = document.querySelector('#currentRainfall');
+
+const picture = document.querySelector('#image');
+
+fetch('http://localhost:8000/data')
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((weather) => {
+      const temp = `<h1>${weather.temperature.temp}C</h1>`;
+      const sky = `<h1>${weather.sky}</h1>`;
+      const info = `<h6 class="card-subtitle mb-2 text-muted"><i class="fa-solid fa-arrow-up"></i> ${weather.temperature.highTemp}C / <i class="fa-solid fa-arrow-down"></i> ${weather.temperature.lowTemp}C Feels like ${weather.temperature.feelsLike}C</h6>`;
+      const uv = `<h5 class="card-title">${weather.uv || 0}</h5>`;
+      const hum = `<h5 class="card-title">${weather.temperature.humidity}</h5>`;
+      const windInfo = `<h5 class="card-title">${weather.wind.direction} ${weather.wind.windSpeed}mph</h5>`;
+      const dewP = `<h5 class="card-title">${weather.temperature.dewpoint}C</h5>`;
+      const pressMB = `<h5 class="card-title">${weather.pressure}mb</h5>`;
+      const rFall = `<h5 class="card-title">${weather.rainfall}mm</h5>`;
+      const aQ = `<h5 class="card-title">${weather.airQuality} AQI</h5>`;
+
+      const image = `<img src=${weather.image} class="img-fluid rounded" alt="Responsive image">`;
+
+      currentTemp.insertAdjacentHTML('beforeend', temp);
+      currentSky.insertAdjacentHTML('beforeend', sky);
+      tempInfo.insertAdjacentHTML('beforeend', info);
+      airQinfo.insertAdjacentHTML('beforeend', aQ);
+      uvIndex.insertAdjacentHTML('beforeend', uv);
+      humidity.insertAdjacentHTML('beforeend', hum);
+      wind.insertAdjacentHTML('beforeend', windInfo);
+      dewpoint.insertAdjacentHTML('beforeend', dewP);
+      pressure.insertAdjacentHTML('beforeend', pressMB);
+      rainfall.insertAdjacentHTML('beforeend', rFall);
+
+      picture.insertAdjacentHTML('beforeend', image);
+    });
+  })
+  .catch((err) => console.log(err));
